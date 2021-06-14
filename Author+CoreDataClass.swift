3d1e5898aa442b2ba76a1b392824985be2f29053
max030511 <max030511@gmail.com>
2021-06-14 09:52:13 -0700

    done

---
+++ Author+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Author+CoreDataClass.swift
+//  CommitsApp
+//
+//  Created by Maxim on 14.06.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Author)
+public class Author: NSManagedObject {
+
+}
---
+++ Commit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Commit+CoreDataClass.swift
+//  CommitsApp
+//
+//  Created by Maxim on 14.06.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Commit: NSManagedObject {
+
+}
